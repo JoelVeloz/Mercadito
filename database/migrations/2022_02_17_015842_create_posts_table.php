5ad57e5b09ac9cd12497c7a9f05e0e984762cf19
porrasmdz <aporrasmdz@gmail.com>
2022-02-16 21:11:24 -0500

    creado post

---
+++ database/migrations/2022_02_17_015842_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->decimal("price");
+            $table->string("title");
+            $table->json("data");
+            $table->foreignId("user_id");
+            $table->foreignId("category_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
