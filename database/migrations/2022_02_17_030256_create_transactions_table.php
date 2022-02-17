05a57e428ea6a6ba24791c4aaa96b2d3a4a1fd6b
porrasmdz <aporrasmdz@gmail.com>
2022-02-16 22:10:03 -0500

    Migraciones hechas

---
+++ database/migrations/2022_02_17_030256_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal("amount");
+            $table->boolean("confirmed");
+            $table->json("meta");
+
+            $table->unsignedBigInteger("wallet_id");
+            $table->foreign("wallet_id")->references("id")->on("wallets");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
